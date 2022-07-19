70aa8ddba6bd987f3963a1865089bb063fbdc78e
washington gama <wgamaneto16@gmail.com>
2022-07-19 16:38:32 -0300

    Requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -22,15 +22,22 @@ const circle = require('../src/circle');
 */
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado possui 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
-    // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
+  it('Teste se circle retorna undefined, caso o parâmetro passado não seja um número.', () => {
+    expect(circle('arroz')).toBe(undefined);
   });
-});
+  it('Teste se circle retorna undefined, caso o parâmetro passado não seja um número.', () => {
+    expect(circle()).toBe(undefined)
+    });
+  it('Teste se circle retorna undefined, caso o parâmetro passado não seja um número.', () => {
+    expect(typeof circle(1)).toBe('object');
+    });
+  it('Teste se circle retorna undefined, caso o parâmetro passado não seja um número.', () => {
+    expect(circle(3).radius).toBeCloseTo(3);
+    });
+  it('Teste se circle retorna undefined, caso o parâmetro passado não seja um número.', () => {
+    expect(circle(2).circumference).toBeCloseTo(12.56);
+    });
+  it('Teste se circle retorna undefined, caso o parâmetro passado não seja um número.', () => {
+    expect(circle(3).area).toBeCloseTo(28.26);
+    });
+  });
